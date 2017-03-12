fa49292ce2b8e90675636df06a79fcd077b99de8
Aditya Mukerjee <dev@chimeracoder.net>
2017-03-12 14:51:41 -0400

    List random experiences

--- parser.go
+++ parser.go
@@ -1,11 +1,18 @@
 package erowidparser
 
 import (
-	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
+
+var rnd = rand.New(rand.NewSource(time.Now().Unix()))
 
 var rel_path = filepath.Join("e", "http80", "www.erowid.org", "experiences")
 
@@ -13,15 +20,69 @@ var root string
 
 var experienceRegexp = regexp.MustCompile(`exp\.phpquery=ID=\d*.html`)
 
-func listExperiences() error {
+func listExperiences() ([]os.FileInfo, error) {
+	var experiences []os.FileInfo
 	files, err := ioutil.ReadDir(filepath.Join(root, rel_path))
 	if err != nil {
-		return err
+		return nil, err
 	}
 	for _, f := range files {
 		if experienceRegexp.MatchString(f.Name()) {
-			fmt.Println(f.Name())
+			experiences = append(experiences, f)
 		}
 	}
-	return err
+	return experiences, err
+}
+
+func parseExperience(filename string) (string, error) {
+	const startPrefix = `<!--   Start Body   -->`
+	const endSuffix = `Exp Year`
+
+	f, err := os.Open(filepath.Join(root, rel_path, filename))
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	doc, err := goquery.NewDocumentFromReader(f)
+	if err != nil {
+		return "", err
+	}
+
+	selection := doc.Find(".report-text-surround")
+	html, err := selection.Html()
+	if err != nil {
+		return "", err
+	}
+
+	i := strings.Index(html, startPrefix)
+	if i > 0 {
+		html = html[i+len(startPrefix):]
+	}
+
+	i = strings.Index(html, endSuffix)
+	if i > 0 {
+		html = html[:i]
+	}
+
+	doc, err = goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		return "", err
+	}
+
+	return doc.Text(), nil
+}
+
+func randExperiences() ([]os.FileInfo, error) {
+	experiences, err := listExperiences()
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]os.FileInfo, len(experiences))
+	list := rnd.Perm(len(experiences))
+	for i, experience := range experiences {
+		result[list[i]] = experience
+	}
+	return result, nil
 }
--- parser_test.go
+++ parser_test.go
@@ -1,9 +1,32 @@
 package erowidparser
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestListExperiences(t *testing.T) {
-	err := listExperiences()
+	files, err := listExperiences()
 	assert.NoError(t, err)
+	assert.True(t, len(files) > 0)
+}
+
+func TestParseExperiences(t *testing.T) {
+	files, err := listExperiences()
+	assert.NoError(t, err)
+	assert.True(t, len(files) > 0)
+
+	file := files[0]
+	_, err = parseExperience(file.Name())
+	assert.NoError(t, err)
+}
+
+func TestRandExperiences(t *testing.T) {
+	exps, err := randExperiences()
+	assert.NoError(t, err)
+	for _, exp := range exps {
+		fmt.Println(exp.Name())
+	}
 }
